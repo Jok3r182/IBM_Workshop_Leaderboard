a3e78dade76d93bcc7b077ce22945f59fd7fad19
Matas Dregva <Matas.Dregva@ibm.com>
2024-06-25 15:03:44 +0300

    Initial commit

---
+++ IBM_Workshop_Leaderboard/IBM_Workshop_LeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBM_Workshop_LeaderboardApp.swift
+//  IBM_Workshop_Leaderboard
+//
+//  Created by Matas Paulius Dregva on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct IBM_Workshop_LeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
